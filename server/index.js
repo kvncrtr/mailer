b383b1f98e6cc07b01b3306d0f334ee1af9ebcbb
Kevin Carter <console.logkevin@gmail.com>
2023-08-11 06:26:36 -0400

    deploy app on render

--- server/index.js
+++ server/index.js
@@ -5,21 +5,30 @@ const keys = require('./config/keys')
 
 const app = express()
 
-passport.use(new GoogleStrategy({
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-   }, (accessToken) => {
-      console.log(accessToken)
-   })
-)
+app.get('/', (req, res)=> {
+   res.send({ hi: 'there' });
+});
 
-app.get('/auth/google', passport.authenticate('google', {
-   scope: ['profile', 'email']
-}))
+// passport.use(new GoogleStrategy({
+//       clientID: keys.googleClientID,
+//       clientSecret: keys.googleClientSecret,
+//       callbackURL: '/auth/google/callback'
+//    }, (accessToken) => {
+//       console.log(accessToken)
+//    })
+// )
+
+// app.get('/auth/google', passport.authenticate('google', {
+//    scope: ['profile', 'email']
+// }))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
+
+// http://localhost:5000/
+
+
+
 
 
 
