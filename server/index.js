2505cd314033088e4ee03407195283cf1f1ffd37
Kevin Carter <console.logkevin@gmail.com>
2023-09-01 15:54:06 -0400

    add auth, passport, mongodb, and mongoose to connect mongodb cloud to local enviorment

--- server/index.js
+++ server/index.js
@@ -1,57 +1,15 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy
 const keys = require('./config/keys')
+require('./models/User');
+require('./services/passport');
 
-const app = express()
+const mongoose = require('mongoose');
 
-passport.use(
-   new GoogleStrategy(
-      {
-         clientID: keys.googleClientID,
-         clientSecret:keys.googleClientSecret,
-         callbackURL: '/auth/google/callback'
-      }, 
-      accessToken => {
-         console.log(accessToken)
-      }
-   )
-);
- 
-app.get('/auth/google', passport.authenticate('google', {
-   scope: ['profile', 'email']
-})) 
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-
-http://localhost:5000/auth/google/callback?code=4%2F0AbUR2VMO0qiQszPgEzbulxgG8UTKZfCAbGvH3io8uXycNsgv35mAuyqmVgnE7Qf3mMYg2A&scope=email+profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+openid&authuser=0&prompt=consent
-
-*/ 
+app.listen(PORT);
---
+++ server/services/passport.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.use(
+   new GoogleStrategy(
+      {
+         clientID: keys.googleClientID,
+         clientSecret:keys.googleClientSecret,
+         callbackURL: '/auth/google/callback'
+      }, 
+      (accessToken, refreshToken, profile, done) => {
+         new User({ googleId: profile.id }).save()
+      }
+   )
+);
