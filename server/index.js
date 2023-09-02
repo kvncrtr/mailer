5e28a7e74199a83794a32c84c2d5906d6b95339d
Kevin Carter <console.logkevin@gmail.com>
2023-09-02 07:46:40 -0400

    add auth cookie sessions to log in and save cookie session

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
-const keys = require('./config/keys')
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
-
-const mongoose = require('mongoose');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+   cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey]
+   })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
