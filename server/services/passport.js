5c7f7295d2598090af12c74dd574b6ea66f7242a
Kevin Carter <console.logkevin@gmail.com>
2023-09-01 21:44:44 -0400

    add a conditional to determine wether if we have that user google id or not in our db

--- server/services/passport.js
+++ server/services/passport.js
@@ -13,7 +13,18 @@ passport.use(
          callbackURL: '/auth/google/callback'
       }, 
       (accessToken, refreshToken, profile, done) => {
-         new User({ googleId: profile.id }).save()
+         User.findOne({ googleId: profile.id })
+         .then((existingUser) => {
+            if (existingUser) {
+               // we already have a record with the given Id 
+               done(null, existingUser);
+            } else {
+               // we dont have a record
+               new User({ googleId: profile.id }).save()
+               .then(user => done(null, user));
+            }
+         })
+
       }
    )
 );
