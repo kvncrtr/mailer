109a8fd1d43c71a8b448cf90487aa9959c0f7155
Kevin Carter <console.logkevin@gmail.com>
2023-09-04 10:57:42 -0400

    add set prod keys and dev keys and an conditional to determin which set of keys to use

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+   googleClientID: "453434982408-o89uj4ohl2kq6dec52n1pjh9jg7ccd54.apps.googleusercontent.com",
+   googleClientSecret: "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",
+   mongoURI: "mongodb+srv://kevincarter:h79xjSoKuznG9gzi@mailercluster.n4yfk02.mongodb.net/mailerdb?retryWrites=true&w=majority",
+   cookieKey: '3f2b87yog4783qobf4yiq3gf467qifb47q3ifg463q7yfg4i3q6ig4b76fqg478q3ogf467iqfb43qgf8743iqbf7483qf4783qfb4378iq'
+}
