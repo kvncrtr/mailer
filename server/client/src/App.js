3582306e4a3a4115fb88e2d60ac1f86780f4de65
Kevin Carter <console.logkevin@gmail.com>
2023-10-27 10:59:59 -0400

    add proxy setup and redirect uri to dev console

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -6,16 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          jesus christ
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>jesus christ</p>
+        <a href="/auth/google">
+          Sign In With Google
         </a>
       </header>
     </div>
