230c7b453f81cabc8f172a7304364e05de112b7c
Kevin Carter <console.logkevin@gmail.com>
2023-10-28 19:40:43 -0400

    refactor passport js and reset entry files for client side

--- server/services/passport.js
+++ server/services/passport.js
@@ -23,19 +23,13 @@ passport.use(
          callbackURL: '/auth/google/callback',
          proxy: true
       }, 
-      (accessToken, refreshToken, profile, done) => {
-         User.findOne({ googleId: profile.id })
-         .then((existingUser) => {
-            if (existingUser) {
-               // we already have a record with the given Id 
-               done(null, existingUser);
-            } else {
-               // we dont have a record
-               new User({ googleId: profile.id }).save()
-               .then(user => done(null, user));
-            }
-         })
-
+      async (accessToken, refreshToken, profile, done) => {
+         const existingUser = await User.findOne({ googleId: profile.id })
+         
+         if (existingUser) return done(null, existingUser);
+          
+         const user = await new User({ googleId: profile.id }).save()
+         done(null, user);
       }
    )
 );
