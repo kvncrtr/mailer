2b663c58d7f1b8d2a32e3a5010106f58135f23ac
Kevin Carter <console.logkevin@gmail.com>
2023-10-28 20:51:52 -0400

    create app.js file and server the js file to the index.js file as a root file

---
+++ server/client/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import { App } from './components/App';
+
+const el = document.getElementById('root');
+
+const root = ReactDOM.createRoot(el);
+
+root.render(<App />);
