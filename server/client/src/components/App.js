cd018484c4d5dfb5fc66a72a8239258555bfdcf3
Kevin Carter <console.logkevin@gmail.com>
2023-11-04 08:35:50 -0400

    hook redux-thunk to action creation and dispatch an action with a react class lifecycle method to  dispact the action when the application is initialized

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 /* Components */
 import Header from './Header';
 
-const Dashboard = () => <h1>Dashboard</h1>
-const SurveyNew = () => <h1>SurveyNew</h1>
-const Landing = () => <h1>Landing</h1>
+const Dashboard = () => <h1>Dashboard</h1>;
+const SurveyNew = () => <h1>SurveyNew</h1>;
+const Landing = () => <h1>Landing</h1>;
 
-const App = () => {
-   return (
-      <div>
-         <BrowserRouter>
-            <div className='container'>
-               <Header />
-               <Route exact path="/" component={Landing} />
-               <Route exact path="/surveys"  component={Dashboard} />
-               <Route path="/surveys/new" component={SurveyNew} />
-            </div>
-         </BrowserRouter> 
-      </div>
-   );
-};
+class App extends Component {
+   componentDidMount() {
+      this.props.fetchUser();
+   };
 
-export default App;
+   render() {
+      return (
+         <div>
+            <BrowserRouter>
+               <div className='container'>
+                  <Header />
+                  <Route exact path="/" component={Landing} />
+                  <Route exact path="/surveys"  component={Dashboard} />
+                  <Route path="/surveys/new" component={SurveyNew} />
+               </div>
+            </BrowserRouter> 
+         </div>
+      );
+   };
+}; 
+
+export default connect(null, actions)(App);
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 root.render(
    <Provider store={store}>
