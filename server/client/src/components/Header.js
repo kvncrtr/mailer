6611de6e5c5e9a81bd4d60c34bd4cb34c5346ce8
Kevin Carter <console.logkevin@gmail.com>
2023-11-04 07:22:55 -0400

    edit setting.json for emmet not working

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class Header extends Component {
    render() {
       return (
-         <div>
-            Header
-         </div>
+         <nav>
+            
+         </nav>
       );
    };
 };
