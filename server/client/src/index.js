99d3f9e0724764166a01c1fe34f84dc8f81b41c6
Kevin Carter <console.logkevin@gmail.com>
2023-11-03 22:36:01 -0400

    commit change to file

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,4 +1,4 @@
-import materializeCSS from 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/css/materialize.min.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
