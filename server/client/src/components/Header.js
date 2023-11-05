68100658bbfecab8dac6964aded0c62ab276a9af
Kevin Carter <console.logkevin@gmail.com>
2023-11-04 23:10:53 -0400

    add link tag to logo

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
    renderContent() {
@@ -17,9 +18,12 @@ class Header extends Component {
       return (
          <nav>
             <div className="nav-wrapper">
-               <a className='left brand-logo'>
+               <Link 
+                  to={this.props.user ? '/surveys' : '/'} 
+                  className='left brand-logo'
+               >
                   Mailer
-               </a>
+               </Link>
 
                <ul className="right">
                   {this.renderContent()}
