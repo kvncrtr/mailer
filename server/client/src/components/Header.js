998127639311fbaa590f60e74be16f9bb3117f46
Kevin Carter <console.logkevin@gmail.com>
2023-11-11 08:28:15 -0500

    create token for payments through stripe, generate a stripe form for accepting card payments

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import Payments from './Payments';
+
 class Header extends Component {
    renderContent() {
       switch(this.props.auth) {
@@ -10,7 +12,10 @@ class Header extends Component {
          case false:
             return <li><a href="/auth/google">Login With Google</a></li>;
          default:
-            return <li><a href='/api/logout'>Logout</a></li>
+            return [  
+               <li><Payments /></li>,
+               <li><a href='/api/logout'>Logout</a></li>
+            ];
       };
    };
 
