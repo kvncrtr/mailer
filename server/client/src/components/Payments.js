fdd22522699322375c8e8ca1102cd7460ef86085
Kevin Carter <console.logkevin@gmail.com>
2023-11-13 08:02:16 -0500

    create new action creator to add an action to when an user submits a payment, create middleware folder and files to transform data, create billing routes to handle request

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 
 class Payments extends Component {
    render() {
@@ -8,15 +11,15 @@ class Payments extends Component {
             name="Mailer"
             description="$5 for 5 email credits"
             amount={500}
-            token={(token) => console.log(token)}
+            token={token => this.props.handleToken(token)}
             stripeKey={process.env.REACT_APP_STRIPE_KEY}
          >
-            <button className="">
+            <button className="btn">
                Add Credits
             </button>
          </StripeCheckout>
       );
-   }
+   };
 };
 
-export default Payments;
+export default connect(null, actions)(Payments);
