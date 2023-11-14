a32aeaf46729a24d9dce3b0ac9694ce1a709937b
Kevin Carter <console.logkevin@gmail.com>
2023-11-14 10:10:13 -0500

    add middleware to routes with end points of '/api/stripe' and update the header with a visual repersentation of how many credits is being used

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -13,8 +13,11 @@ class Header extends Component {
             return <li><a href="/auth/google">Login With Google</a></li>;
          default:
             return [  
-               <li key="1"><Payments /></li>,
-               <li key="2"><a href='/api/logout'>Logout</a></li>
+               <li key='1'><Payments /></li>,
+               <li key='2' style={{ margin: '0 10px'}}>
+                  Credits: {this.props.auth.credits}
+               </li>,
+               <li key='3'><a href='/api/logout'>Logout</a></li>
             ];
       };
    };
