a6d7e205d425afbf6ede823bdbc2933220165543
Kevin Carter <console.logkevin@gmail.com>
2023-12-06 21:54:44 -0500

    add sendgrid module, and api key

--- server/config/dev.js
+++ server/config/dev.js
@@ -4,5 +4,6 @@ module.exports = {
    mongoURI: "mongodb+srv://kevincarter:h79xjSoKuznG9gzi@mailercluster.n4yfk02.mongodb.net/mailerdb?retryWrites=true&w=majority",
    cookieKey: '3f2b87yog4783qobf4yiq3gf467qifb47q3ifg463q7yfg4i3q6ig4b76fqg478q3ogf467iqfb43qgf8743iqbf7483qf4783qfb4378iq',
    stripePublishableKey: 'pk_test_51OAXL9Ih6JfiaS72DCDx6zz9v5YZerk8JoOAL3Vq7d0275tjjyM9NR8foBHP0ODKbi47BVthN8oh2GpZJ6UjdLhN003YBD3RYA',
-   stripeSecretKey: 'sk_test_51OAXL9Ih6JfiaS723JJwQnkeuyWAwlaDJDCyocguhfku83Bkfb76IT1jJIL5AvMnTdZcRgGbSaINK4ojYke4XJJH00wYQ74xDr'
+   stripeSecretKey: 'sk_test_51OAXL9Ih6JfiaS723JJwQnkeuyWAwlaDJDCyocguhfku83Bkfb76IT1jJIL5AvMnTdZcRgGbSaINK4ojYke4XJJH00wYQ74xDr',
+   sendGridKey: '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'
 };
